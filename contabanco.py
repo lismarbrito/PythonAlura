49d4ff0aef6bfdab706a0ab610aa9a4dd1280a04
Lismar Brito <lismar.sousa@serpro.gov.br>
2022-06-20 16:06:03 -0300

    Final do curso Python3

--- contabanco.py
+++ contabanco.py
@@ -15,8 +15,15 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        disponivel_para_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= disponivel_para_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if (self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
 
     def transfere(self, valor, destino):
         self.saca
@@ -43,3 +50,7 @@ class Conta:
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': 237}
