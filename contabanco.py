377eafef47527e4c148b83c51c74409b868c1534
Lismar Brito <lismar.sousa@serpro.gov.br>
2022-06-20 09:22:30 -0300

    Incluindo método de tranferencia

--- contabanco.py
+++ contabanco.py
@@ -1,22 +1,21 @@
-# Aprendendo sobre funções contrutoras
-
-
 class Conta:
-    # Esta é a função contrutora que é chamada automaticamente após a constução do objeto.
-    # "Self" é a referência que sabe o endereço da memória onde está o objeto.
+    
     def __init__(self, numero, titular, saldo, limite):
-        print("Construindo objeto...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        print("Construindo objeto ... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("O saldo de {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
-    def deposito(self, valor):
-        self.saldo += valor
+    def deposita(self, valor):
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
 
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
